efcee12797c39ae5e7d8019c06e6993587e96dc1
Taylor Peet <repeet13@gmail.com>
2013-03-02 23:47:42 -0500

    added login activity

--- MPG Tracker/src/com/unrulyrecursion/mpgtracker/MainActivity.java
+++ MPG Tracker/src/com/unrulyrecursion/mpgtracker/MainActivity.java
@@ -41,4 +41,9 @@ public class MainActivity extends Activity {
     	intent.putExtra(CAR_NAME, carName);
     	startActivity(intent);
     }
+    
+    public void login(View view) {
+    	Intent intent = new Intent(this, LoginActivity.class);
+    	
+    }
 }
