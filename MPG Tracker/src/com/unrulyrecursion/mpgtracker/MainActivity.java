2ad354e9b0e4b488b29f1d49812127be81ee5ac5
Taylor Peet <repeet13@gmail.com>
2013-03-04 17:45:33 -0500

    added graphs activity, small changes as well

--- MPG Tracker/src/com/unrulyrecursion/mpgtracker/MainActivity.java
+++ MPG Tracker/src/com/unrulyrecursion/mpgtracker/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO get current car
+        
     }
 
     @Override
