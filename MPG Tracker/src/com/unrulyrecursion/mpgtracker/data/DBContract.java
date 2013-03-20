48d9e4cc8e33e1a3a4089d263f9280b15cf9cde3
Taylor Peet <repeet13@gmail.com>
2013-03-20 16:11:32 -0400

    pulled database related files from FragTime Branch

---
+++ MPG Tracker/src/com/unrulyrecursion/mpgtracker/data/DBContract.java
@@ -0,0 +1,84 @@
+package com.unrulyrecursion.mpgtracker.data;
+
+import android.provider.BaseColumns;
+
+public class DBContract {
+
+//	Android Training website for Databases:
+//	http://developer.android.com/training/basics/data-storage/databases.html
+	
+	// Empty constructor discourages attempting to instantiate the class
+	private DBContract() {}
+	
+	/*
+	 * Database Schema
+	 * 
+	 * Columns arranged in decreasing order of importance.
+	 * Tables that use global columns have them also, but commented.
+	 */
+	
+	// Global Column names
+	public static final String COLUMN_NAME_CAR_NAME = "carname";
+	public static final String COLUMN_NAME_DATE = "date";
+	public static final String COLUMN_NAME_TOTAL_MILEAGE = "totalmileage";
+	
+	/**
+	 * Contains information on all cars in the system (one row per car). Keyed 
+	 * by Car Name.
+	 * @author Taylor
+	 */
+	public static abstract class Cars implements BaseColumns {
+		public static final String TABLE_NAME = "cars";
+//		public static final String COLUMN_NAME_CAR_NAME = "carname"; //primary key
+		public static final String COLUMN_NAME_COLOR = "color";
+		public static final String COLUMN_NAME_MAKE = "make";
+		public static final String COLUMN_NAME_MODEL = "model";
+		public static final String COLUMN_NAME_YEAR = "year";
+//		public static final String COLUMN_NAME_TOTAL_MILEAGE = "totalmileage";
+	}
+	
+	/**
+	 * Contains information of all fillups that have been recorded. Keyed by 
+	 * a unique id, and Car Name.
+	 * @author Taylor
+	 */
+	public static abstract class Fillups implements BaseColumns {
+		public static final String TABLE_NAME = "fillups";
+		public static final String COLUMN_NAME_ENTRY_ID = "entryid"; //primary key
+//		public static final String COLUMN_NAME_CAR_NAME = "carname";
+//		public static final String COLUMN_NAME_DATE = "date";
+//		public static final String COLUMN_NAME_TOTAL_MILEAGE = "totalmileage";
+		public static final String COLUMN_NAME_TRIP_MILEAGE = "tripmileage";
+		public static final String COLUMN_NAME_GALLONS_IN = "gallonsin";
+		public static final String COLUMN_NAME_PRICE_PER_GALLON = "pricepergallon";
+	}
+	
+	
+	/* Unimplemented sections of DB
+
+	// Used in future implementation with Oil Change Tracking
+	public static abstract class OilChange implements BaseColumns {
+		public static final String TABLE_NAME = "fillups";
+		public static final String COLUMN_NAME_ENTRY_ID = "entryid"; //primary key
+		public static final String COLUMN_NAME_CAR_NAME = "carname";
+		public static final String COLUMN_NAME_DATE = "date";
+		public static final String COLUMN_NAME_TOTAL_MILEAGE = "totalmileage";
+		public static final String COLUMN_NAME_MILES_ON_LAST_OIL = "milesonlastoil";
+		public static final String COLUMN_NAME_OIL_USED = "oilused";
+		public static final String COLUMN_NAME_OIL_PRICE = "oilprice";
+		public static final String COLUMN_NAME_FILTER_USED = "filterused";
+		public static final String COLUMN_NAME_FILTER_PRICE = "filterprice";
+	}
+	
+	// Used in future implementation with Notifications to check maintenance items
+	public static abstract class DefaultCheckTimes implements BaseColumns {
+		public static final String TABLE_NAME = "defaultchecktimes";
+		public static final String COLUMN_NAME_CAR_NAME = "carname"; // generic
+		public static final String COLUMN_NAME_CHANGE_OIL = "changeoil"; // 5000 miles
+		public static final String COLUMN_NAME_CHANGE_WIPERS = "changewipers"; // 6 months
+		public static final String COLUMN_NAME_CHECK_WIPER_FLUID = "checkwiperfluid"; // 3 months
+		public static final String COLUMN_NAME_CHECK_TIRES = "checktires"; // 10,000 miles
+	}
+	
+	*/
+}
---
+++ MPG Tracker/src/com/unrulyrecursion/mpgtracker/data/MPGTrackerDBHelper.java
@@ -0,0 +1,71 @@
+package com.unrulyrecursion.mpgtracker.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MPGTrackerDBHelper extends SQLiteOpenHelper {
+	
+	/*
+	 * If db schema changes, increment the database version
+	 */
+	public static final int DATABASE_VERSION = 1;
+	public static final String DATABASE_NAME = "mpgtracker.db";
+	
+	/*
+	 * Database helper variables
+	 */
+	
+	private static final String COMMA_SEP = ",";
+	
+	// Type Vars
+	private static final String TEXT_TYPE = " TEXT";
+	
+	// Table Create Statements
+	private static final String SQL_CREATE_TABLE_CARS = //TODO Test this. Logcat Debug Level
+			"CREATE TABLE " + DBContract.Cars.TABLE_NAME + " (" +
+			DBContract.COLUMN_NAME_CAR_NAME + TEXT_TYPE + " PRIMARY KEY," + 
+			DBContract.Cars.COLUMN_NAME_COLOR + TEXT_TYPE + COMMA_SEP + 
+			DBContract.COLUMN_NAME_TOTAL_MILEAGE + TEXT_TYPE + COMMA_SEP + 
+			DBContract.Cars.COLUMN_NAME_MAKE + TEXT_TYPE + COMMA_SEP + 
+			DBContract.Cars.COLUMN_NAME_MODEL + TEXT_TYPE + COMMA_SEP + 
+			DBContract.Cars.COLUMN_NAME_YEAR + TEXT_TYPE + COMMA_SEP + " )";
+	
+	private static final String SQL_CREATE_TABLE_FILLUPS = //TODO Test this. Logcat Debug Level
+			"CREATE TABLE " + DBContract.Fillups.TABLE_NAME + " (" + 
+			DBContract.Fillups._ID + " INTEGER PRIMARY KEY," + 
+			DBContract.Fillups.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP + 
+			DBContract.COLUMN_NAME_CAR_NAME + TEXT_TYPE + COMMA_SEP + 
+			DBContract.COLUMN_NAME_DATE + TEXT_TYPE + COMMA_SEP + 
+			DBContract.COLUMN_NAME_TOTAL_MILEAGE + TEXT_TYPE + COMMA_SEP + 
+			DBContract.Fillups.COLUMN_NAME_TRIP_MILEAGE + TEXT_TYPE + COMMA_SEP + 
+			DBContract.Fillups.COLUMN_NAME_GALLONS_IN + TEXT_TYPE + COMMA_SEP + 
+			DBContract.Fillups.COLUMN_NAME_PRICE_PER_GALLON + TEXT_TYPE + COMMA_SEP + " )";
+	
+	
+	public MPGTrackerDBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		Log.v("MPGTrackerDBHelper", "Creating SQLite Database");
+		Log.d("MPGTrackerDBHelper", "Creating Cars Table");
+		Log.i("MPGTrackerDBHelper", "SQL-" + SQL_CREATE_TABLE_CARS);
+		db.execSQL(SQL_CREATE_TABLE_CARS);
+		
+		Log.d("MPGTrackerDBHelper", "Creating Fillups Table");
+		Log.i("MPGTrackerDBHelper", "SQL-" + SQL_CREATE_TABLE_FILLUPS);
+		db.execSQL(SQL_CREATE_TABLE_FILLUPS);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.v("MPGTrackerDBHelper", "Upgrading SQLite Database");
+		// TODO Auto-generated method stub
+		
+	}
+
+}
