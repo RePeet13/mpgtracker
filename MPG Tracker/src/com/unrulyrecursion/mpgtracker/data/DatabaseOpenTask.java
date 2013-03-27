a708370002170637f086146b286542e3a1acd151
Taylor Peet <repeet13@gmail.com>
2013-03-27 11:36:47 -0400

    Added AsyncTask for Database

---
+++ MPG Tracker/src/com/unrulyrecursion/mpgtracker/data/DatabaseOpenTask.java
@@ -0,0 +1,13 @@
+package com.unrulyrecursion.mpgtracker.data;
+
+import android.os.AsyncTask;
+
+public class DatabaseOpenTask extends AsyncTask<String, Integer, Integer> {
+
+	@Override
+	protected Integer doInBackground(String... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
