d400843f6492a2792437fdf5e644ee1297df30aa
Taylor Peet <repeet13@gmail.com>
2013-03-27 11:51:28 -0400

    Added Todos to Database Task file

--- MPG Tracker/src/com/unrulyrecursion/mpgtracker/data/DatabaseOpenTask.java
+++ MPG Tracker/src/com/unrulyrecursion/mpgtracker/data/DatabaseOpenTask.java
@@ -1,12 +1,20 @@
 package com.unrulyrecursion.mpgtracker.data;
 
 import android.os.AsyncTask;
+import java.util.List;
 
-public class DatabaseOpenTask extends AsyncTask<String, Integer, Integer> {
+public class DatabaseOpenTask extends AsyncTask<String, Integer, List<Car>> {
 
+	// TODO overwrite on progress thing
+	
 	@Override
-	protected Integer doInBackground(String... params) {
+	protected List<Car> doInBackground(String... params) {
 		// TODO Auto-generated method stub
+		// TODO Get writable database
+		// TODO handle if there are no cars
+		// TODO Get and make cars from them
+		// TODO progress is cars made/total cars
+		// TODO returns arraylist of cars in carlist
 		return null;
 	}
 
